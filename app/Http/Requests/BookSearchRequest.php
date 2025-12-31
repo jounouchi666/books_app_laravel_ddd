a19620923fcb040f44866e8d5f09cbe262945c3c
jounouchi666 <trueguy666@gmail.com>
2025-12-31 14:24:07 +0900

    FormRequest作成「BookSearch」

--- app/Http/Requests/BookSearchRequest.php
+++ app/Http/Requests/BookSearchRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Application\Book\Query\ListBookQuery;
 use Illuminate\Foundation\Http\FormRequest;
 
 class BookSearchRequest extends FormRequest
@@ -11,7 +12,7 @@ class BookSearchRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,40 @@ class BookSearchRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'user_id'     => ['nullable', 'integer', 'min:1'],
+            'category_id' => ['nullable', 'integer', 'min:1'],
+            'sort'        => ['nullable', 'string', 'in:title,user_id,category_id,created_at'],
+            'direction'   => ['nullable', 'in:desc,asc']
         ];
+    }
+    
+    /**
+     * error messages
+     *
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'user_id.integer'     => '正規のユーザーを入力してください',
+            'category_id.integer' => '正規のカテゴリーを入力してください',
+            'sort.in'             => 'ソート項目が有効ではありません',
+            'direction.in'        => '昇順（desc）または降順（asc）で入力してください'
+        ];
+    }
+    
+    /**
+     * クエリオブジェクトを生成
+     *
+     * @return ListBookQuery
+     */
+    public function buildQuery(): ListBookQuery
+    {
+        return new ListBookQuery(
+            $this->integer('user_id'),
+            $this->integer('category_id'),
+            $this->input('sort'),
+            $this->input('direction')
+        );
     }
 }
