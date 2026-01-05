1b9403ef571f1aed1641166dd73616f2b2231ee7
jounouchi666 <trueguy666@gmail.com>
2026-01-05 16:33:00 +0900

    ルーティング構築

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Books
+    Route::prefix('/books')->name('books.')->group(function() {
+        Route::get('/', [BookController::class, 'index'])->name('index');
+        Route::get('/create', [BookController::class, 'create'])->name('create');
+        Route::get('/{id}', [BookController::class, 'show'])->name('show');
+        Route::get('/{id}/edit', [BookController::class, 'edit'])->name('edit');
+
+        Route::post('/store', [BookController::class, 'store'])->name('store');
+        Route::patch('/{id}/update', [BookController::class, 'update'])->name('update');
+        Route::delete('/{id}/delete', [BookController::class, 'delete'])->name('delete');
+    });
 });
 
 require __DIR__.'/auth.php';
