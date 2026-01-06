832c90e212cbcf4e384bfc4dbd446bcc1ffbd9a4
jounouchi666 <trueguy666@gmail.com>
2026-01-06 16:48:20 +0900

    編集権限のプロパティを追加、Entityからの生成を不可能に

--- app/Application/Book/DTO/BookView.php
+++ app/Application/Book/DTO/BookView.php
@@ -2,12 +2,11 @@
 
 namespace App\Application\Book\DTO;
 
-use App\Domain\Book\Entity\Book;
-use LogicException;
-
 /**
  * DTO
  * BookView
+ * 
+ * 原則Assemblerから作成する
  */
 final class BookView
 {
@@ -16,41 +15,8 @@ final class BookView
         public readonly string $title,
         public readonly int $userId,
         public readonly int $categoryId,
-        public readonly string $categoryTitle
+        public readonly string $categoryTitle,
+        public readonly bool $canUpdate,
+        public readonly bool $canDelete
     ) {}
-
-        
-    /**
-     * エンティティからインスタンスを作成
-     * リレーション先の情報を持たないため、$catagoryTitleは''(空文字)
-     *
-     * @param  Book $book
-     * @return self
-     */
-    public static function fromEntity(Book $book): self
-    {
-        if (is_null($book->id())) throw new LogicException('Book must have id');
-
-        return new self(
-            $book->id()->value(),
-            $book->title()->value(),
-            $book->userId()->value(),
-            $book->categoryId()->value(),
-            '',
-        );
-    }
-    
-    /**
-     * エンティティの配列からインスタンスを作成
-     *
-     * @param  Book[] $books
-     * @return self[]
-     */
-    public static function fromEntities(array $books): array
-    {
-        return array_map(
-            fn(Book $book) => self::fromEntity($book),  
-            $books
-        );
-    }
 }
