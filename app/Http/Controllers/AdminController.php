c77007b04f0c9ca7a039be2ca2a44f76cb3a5210
jounouchi666 <trueguy666@gmail.com>
2026-01-11 12:51:21 +0900

    コントローラー作成「AdminController」

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class AdminController extends Controller
+{
+        
+    /**
+     * index
+     *
+     * @return View
+     */
+    public function index(): View
+    {
+        return view('admin.index');
+    }
+}
