b1738da1a061cfac2055dc2e3511c8b705db20e2
jounouchi666 <trueguy666@gmail.com>
2026-01-13 00:26:12 +0900

    書籍詳細表示

---
+++ resources/views/books/show.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ $book->title }}
+        </h2>
+    </x-slot>
+    
+    <div class="max-w-7xl mx-auto px-6 py-4 mt-4 bg-white">
+        <h1 class="text-3xl font-medium">{{ $book->title }}</h1>
+
+        <div class="mt-4 w-full flex flex-col gap-2">
+            <p>{{ $book->categoryTitle }}</p>
+        </div>
+    </div>
+</x-app-layout>
