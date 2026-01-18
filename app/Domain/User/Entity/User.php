f11ec329ae0f58f8683223c9bc98b9336ef1c294
jounouchi666 <trueguy666@gmail.com>
2026-01-18 14:45:27 +0900

    インターフェースの適用、メソッド実装

--- app/Domain/User/Entity/User.php
+++ app/Domain/User/Entity/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Domain\User\Entity;
 
+use App\Domain\Auth\AuthorizableResource;
 use App\Domain\Shared\ValueObject\UserId;
 use App\Domain\User\ValueObject\Name;
 
@@ -9,13 +10,35 @@ use App\Domain\User\ValueObject\Name;
  * エンティティ
  * User
  */
-final class User
+final class User implements AuthorizableResource
 {
     private function __construct(
         private readonly ?UserId $id,
         private Name $name,
         private bool $isAdmin
     ) {}
+
+    /**
+     * 認可対象キー
+     * Modelを取得するために使用
+     *
+     * @return int|string
+     */
+    public function authorizationKey(): int|string
+    {
+        return $this->id()->value();
+    }
+
+    /**
+     * 認可対象タイプ
+     * Model判別に使用
+     *
+     * @return string
+     */
+    public function authorizationType(): string
+    {
+        return 'user';
+    }
 
     /**
      * 新規作成
