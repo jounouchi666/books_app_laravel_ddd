5d2928e2306f5c1bb008215541d91f1a8f3b800a
jounouchi666 <trueguy666@gmail.com>
2026-01-18 15:35:07 +0900

    中央揃え追加

--- resources/views/components/danger-button.blade.php
+++ resources/views/components/danger-button.blade.php
@@ -2,7 +2,7 @@
     {{
         $attributes->merge([
             'type' => 'submit',
-            'class' => 'inline-flex items-center px-4 h-10 bg-red-600 text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 hover:cursor-pointer select-none transition-colors'
+            'class' => 'inline-flex items-center justify-center px-4 h-10 bg-red-600 text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 hover:cursor-pointer select-none transition-colors'
         ])
     }}
 >
