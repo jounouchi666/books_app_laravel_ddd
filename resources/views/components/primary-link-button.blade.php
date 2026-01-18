0589dc82428dbacf26c8d457da58e250ee482ee6
jounouchi666 <trueguy666@gmail.com>
2026-01-18 15:09:32 +0900

    Bladeコンポーネント作成「primary-link-button」

---
+++ resources/views/components/primary-link-button.blade.php
@@ -0,0 +1,7 @@
+<a {{
+    $attributes->merge([
+        'class' => 'inline-flex items-center justify-center px-4 h-10 bg-neutral-600 dark:bg-neutral-200 text-white dark:text-gray-800 uppercase tracking-widest hover:bg-neutral-500 dark:hover:bg-white focus:bg-neutral-700 dark:focus:bg-white hover:cursor-pointer select-none transition-colors'
+    ]) 
+}} >
+    {{ $slot }}
+</a>
