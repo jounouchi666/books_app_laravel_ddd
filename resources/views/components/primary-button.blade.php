011b16a4bf5e4199d7adab7203b51ecc4985b83a
jounouchi666 <trueguy666@gmail.com>
2026-01-18 15:10:21 +0900

    レイアウト調整

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,6 +1,6 @@
 <button {{ $attributes->merge([
     'type' => 'submit',
-    'class' => 'inline-flex items-center px-4 h-10 bg-neutral-600 dark:bg-neutral-200 text-white dark:text-gray-800 uppercase tracking-widest hover:bg-neutral-500 dark:hover:bg-white focus:bg-neutral-700 dark:focus:bg-white active:bg-neutral-950 dark:active:bg-neutral-300 hover:cursor-pointer transition-colors'
+    'class' => 'inline-flex items-center justify-center px-4 h-10 bg-neutral-600 dark:bg-neutral-200 text-white dark:text-gray-800 uppercase tracking-widest hover:bg-neutral-500 dark:hover:bg-white focus:bg-neutral-700 dark:focus:bg-white hover:cursor-pointer select-none transition-colors'
 ]) }}>
     {{ $slot }}
 </button>
