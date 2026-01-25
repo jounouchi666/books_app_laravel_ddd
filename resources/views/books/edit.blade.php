6f3b01a2b942ccb9281de6edf3781fa2d9f1b457
jounouchi666 <trueguy666@gmail.com>
2026-01-25 13:45:01 +0900

    Bladeコンポーネントの適用

--- resources/views/books/edit.blade.php
+++ resources/views/books/edit.blade.php
@@ -5,41 +5,47 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <form
-                    action="{{ $mode === 'create' ? route('books.store') : route('books.update', ['id' => $book->id]) }}"
-                    method="POST"
-                    class="mt-8 flex flex-col items-center"
-                >
-                    @csrf
-                    @if ($mode === 'edit')
-                        @method('patch')
-                    @endif
-                    <div class="mt-4 w-full flex flex-col gap-2">
-                        <label class="text-lg leading-none" for="book_title">タイトル</label>
-                        <input id="book_title" class="px-3 outline h-10" type="text" name="title" value="{{ old('title', $book->title) }}" maxlength="100" required>
-                    </div>
+    <x-main-content-area>
+        <x-main-content-panel>
+            <form
+                action="{{ $mode === 'create' ? route('books.store') : route('books.update', ['id' => $book->id]) }}"
+                method="POST"
+                class="mt-8 flex flex-col items-center"
+            >
+                @csrf
+                @if ($mode === 'edit')
+                    @method('patch')
+                @endif
+                <div class="mt-4 w-full flex flex-col gap-2">
+                    <x-input-label for="book_title" value="タイトル"/>
+                    <x-text-input
+                        id="book_title"
+                        type="text"
+                        name="title"
+                        :value="old('title', $book->title)"
+                        maxlength="100"
+                        required
+                    />
+                    <x-input-error :messages="$errors->get('title')" class="mt-2" />
+                </div>
 
-                    <div class="mt-4 w-full flex flex-col gap-2">
-                        <label for="book_category">カテゴリー</label>
-                        <select id="book_category" class="px-3 outline h-10" name="category_id">
-                            <option value="">カテゴリーを選択してください</option>
-                            @foreach ($categories as $category)
-                                <option
-                                    value="{{ $category->id }}"
-                                    {{ old('category_id', $book->categoryId ?? '') === $category->id ? 'selected' : '' }}
-                                >
-                                    {{ $category->name }}
-                                </option>
-                            @endforeach
-                        </select>
-                    </div>
-                    
-                    <input class="mt-4 w-48 outline h-10 bg-neutral-600 text-white hover:bg-neutral-500 hover:cursor-pointer transition-colors" type="submit" value="登録">
-                </form>
-            </div>
-        </div>
-    </div>
+                <div class="mt-4 w-full flex flex-col gap-2">
+                    <x-input-label for="book_category" value="カテゴリー" />
+                    <x-select-input
+                        id="book_category"
+                        name="category_id"
+                        :options="$categories"
+                        :selected="old('category_id', $book->categoryId)"
+                    >
+                        <option value="">カテゴリーを選択してください</option>
+                    </x-select-input>
+                    <x-input-error :messages="$errors->get('category_id')" class="mt-2" />
+                </div>
+                
+                <x-primary-button class="mt-8 w-48">
+                    登録
+                </x-primary-button>
+            </form>
+        </x-main-content-panel>
+    </x-main-content-area>
 </x-app-layout>
