1ff13f198193dd544dac7a2fc9dcf86efc024db6
jounouchi666 <trueguy666@gmail.com>
2026-01-25 13:23:01 +0900

    URLジェネレーターを適用

--- app/Application/UI/DTO/PaginateView.php
+++ app/Application/UI/DTO/PaginateView.php
@@ -2,6 +2,8 @@
 
 namespace App\Application\UI\DTO;
 
+use App\Application\UI\PaginationUrlGenerator;
+
 /**
  * DTO
  * PaginateView
@@ -17,8 +19,7 @@ final class PaginateView
         public readonly int $total,
         public readonly bool $onFirstPage,
         public readonly bool $onLastPage,
-        public readonly ?string $nextPageUrl,
-        public readonly ?string $previousPageUrl
+        private readonly PaginationUrlGenerator $urlGenerator
     ) {}
 
     /**
@@ -28,6 +29,57 @@ final class PaginateView
      */
     public function hasPagination(): bool
     {
-        return !$this->onFirstPage || !$this->onLastPage;
+        return $this->onLastPage > 1;
+    }
+    
+    /**
+     * URLを生成
+     *
+     * @param  int $page
+     * @return string
+     */
+    public function pageUrl(int $page): string
+    {
+        return $this->urlGenerator->pageUrl($page);
+    }
+        
+    /**
+     * 前のページのURLを生成
+     *
+     * @return string
+     */
+    public function previousPageUrl(): ?string
+    {
+        return $this->urlGenerator->previousPageUrl();
+    }
+
+    /**
+     * 次のページのURLを生成
+     *
+     * @return string
+     */
+    public function nextPageUrl(): ?string
+    {
+        return $this->urlGenerator->nextPageUrl();
+    }
+
+    /**
+     * 最初のページのURLを生成
+     *
+     * @return string
+     */
+    public function firstPageUrl(): ?string
+    {
+        return $this->urlGenerator->firstPageUrl();
+    }
+
+    /**
+     * 最後のページのURLを生成
+     *
+     * @return string
+     */
+    public function lastPageUrl(): ?string
+    {
+        return $this->urlGenerator->lastPageUrl();
     }
 }
