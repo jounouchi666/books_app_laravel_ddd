514a6c3b838e95c4f77f9c898c6f8179af42d706
jounouchi666 <trueguy666@gmail.com>
2026-02-09 15:15:53 +0900

    CategorySeeder作成

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            ['title' => '小説'],
+            ['title' => 'ビジネス・経済'],
+            ['title' => '自己啓発'],
+            ['title' => 'IT・プログラミング'],
+            ['title' => '科学・技術'],
+            ['title' => '歴史・地理'],
+            ['title' => '社会・政治'],
+            ['title' => '芸術・デザイン'],
+            ['title' => '趣味・実用'],
+            ['title' => '教育・学習'],
+        ];
+
+        $now = Carbon::now();
+
+        DB::table('categories')->insert(
+            array_map(
+                fn ($category) => [
+                    'title' => $category['title'],
+                    'created_at' => $now,
+                    'updated_at' => $now
+                ],
+                $categories
+            )
+        );
+    }
+}
