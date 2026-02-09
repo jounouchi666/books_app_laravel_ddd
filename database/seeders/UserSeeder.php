9c1206abf890c689714f6029c8b30ba4c8854c2f
jounouchi666 <trueguy666@gmail.com>
2026-02-09 17:39:47 +0900

    モデルベースに変更

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -16,7 +16,7 @@ class UserSeeder extends Seeder
     {
         $now = Carbon::now();
 
-        DB::table('users')->updateOrInsert(
+        User::updateOrInsert(
             ['email' => env('ADMIN_EMAIL', 'admin@example.com')],
             [
             'name' => 'admin',
