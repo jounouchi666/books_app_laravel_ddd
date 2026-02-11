dbb093375f9045be72ca91bf571c7a2e958af9ae
jounouchi666 <trueguy666@gmail.com>
2026-02-11 19:58:24 +0900

    削除モーダル実装

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -35,61 +35,99 @@
                 </div>
             </div>
 
-            <table class="mt-2 w-full table-auto border-collapse border border-neutral-200">
-                <thead>
-                    <tr class="dark:border dark:border-neutral-200">
-                        <th class="w-32 bg-neutral-600 p-2 text-white text-left">ID</th>
-                        <th class="bg-neutral-600 p-2 text-white text-left">タイトル</th>
-                        <th class="bg-neutral-600 p-2 text-white text-left">カテゴリー</th>
-                        <th class="w-16 bg-neutral-600 p-2 text-white text-left">編集</th>
-                        <th class="w-16 bg-neutral-600 p-2 text-white text-left">削除</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @if (!empty($books->bookViews))
-                        @foreach($books->bookViews as $book)
-                            <tr class="border border-neutral-200">
-                                <td class="p-2 text-gray-800 dark:text-gray-200">{{ $book->id }}</td>
-                                <td class="p-2 text-gray-800 dark:text-gray-200">
-                                    <a
-                                        class="focus:underline{{ $book->trashed ? ' line-through text-red-600' : '' }}"
-                                        href="{{ route('books.show', ['id' => $book->id]) }}"
-                                    >
-                                        {{ $book->title }}
-                                    </a>
-                                </td>
-                                <td class="p-2 text-gray-800 dark:text-gray-200">{{ $book->categoryTitle }}</td>
-                                <td class="p-2 text-gray-800 dark:text-gray-200">
-                                    @if ($book->canUpdate)
-                                    <div>
-                                        <a class="hover:text-neutral-500 transition-colors" href="{{ route('books.edit', ['id' => $book->id]) }}">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-pen-icon lucide-square-pen"><path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z"/></svg>
+            <div
+                x-data="{ deleteAction: '', title: '' }"
+                x-on:open-book-delete-modal.window="
+                    deleteAction = $event.detail.action
+                    title = $event.detail.title
+                "
+            >
+                <table class="mt-2 w-full table-auto border-collapse border border-neutral-200">
+                    <thead>
+                        <tr class="dark:border dark:border-neutral-200">
+                            <th class="w-32 bg-neutral-600 p-2 text-white text-left">ID</th>
+                            <th class="bg-neutral-600 p-2 text-white text-left">タイトル</th>
+                            <th class="bg-neutral-600 p-2 text-white text-left">カテゴリー</th>
+                            <th class="w-16 bg-neutral-600 p-2 text-white text-left">編集</th>
+                            <th class="w-16 bg-neutral-600 p-2 text-white text-left">削除</th>
+                        </tr>
+                    </thead>
+                    <tbody class="border border-neutral-200">
+                        @if (!empty($books->bookViews))
+                            @foreach($books->bookViews as $book)
+                                <tr>
+                                    <td class="p-2 text-gray-800 dark:text-gray-200">{{ $book->id }}</td>
+                                    <td class="p-2 text-gray-800 dark:text-gray-200">
+                                        <a
+                                            class="focus:underline{{ $book->trashed ? ' line-through text-red-600' : '' }}"
+                                            href="{{ route('books.show', ['id' => $book->id]) }}"
+                                        >
+                                            {{ $book->title }}
                                         </a>
-                                    </div>
-                                    @endif
-                                </td>
-                                <td class="p-2 text-gray-800 dark:text-gray-200">
-                                    @if ($book->canDelete)
-                                    <form action="{{ route('books.delete', ['id' => $book->id]) }}" method="post">
-                                        @csrf
-                                        @method('delete')
-                                        <button class="hover:text-neutral-500 transition-colors" type="submit" x-data x-on:click="$dispatch('open-modal', 'confirm-delete')">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-icon lucide-trash"><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
-                                        </button>
-                                    </form>
-                                    @endif
-                                </td>
-                            </tr>
-                        @endforeach
-                    @endif
-                </tbody>
-            </table>
+                                    </td>
+                                    <td class="p-2 text-gray-800 dark:text-gray-200">{{ $book->categoryTitle }}</td>
+                                    <td class="p-2 text-gray-800 dark:text-gray-200">
+                                        @if ($book->canUpdate)
+                                        <div>
+                                            <a class="hover:text-neutral-500 transition-colors" href="{{ route('books.edit', ['id' => $book->id]) }}">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-pen-icon lucide-square-pen"><path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z"/></svg>
+                                            </a>
+                                        </div>
+                                        @endif
+                                    </td>
+                                    <td class="p-2 text-gray-800 dark:text-gray-200">
+                                        @if ($book->canDelete)
+                                        <div>
+                                            <button
+                                                class="hover:text-neutral-500 transition-colors"
+                                                type="button"
+                                                x-on:click="
+                                                    $dispatch('open-book-delete-modal', {
+                                                        action: '{{ route('books.delete', ['id' => $book->id]) }}',
+                                                        title: '{{ $book->title }}'
+                                                    });
+                                                    $dispatch('open-modal', 'confirm-delete');
+                                                "
+                                            >
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-icon lucide-trash">
+                                                    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/>
+                                                    <path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
+                                                </svg>
+                                            </button>
+                                        </div>
+                                        @endif
+                                    </td>
+                                </tr>
+                            @endforeach
+                        @endif
+                    </tbody>
+                </table>
+
+                <x-modal name="confirm-delete" focusable>
+                    <form
+                        method="post"
+                        class="p-6"
+                        x-bind:action="deleteAction"
+                    >
+                        @csrf
+                        @method('delete')
+
+                        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100" x-text="`${title} を本当に削除しますか？`"></h2>
+
+                        <div class="mt-6 flex justify-end">
+                            <x-secondary-button x-on:click="$dispatch('close-modal', 'confirm-delete')">
+                                キャンセル
+                            </x-secondary-button>
+
+                            <x-danger-button class="ms-3" x-bind:disabled="!deleteAction">
+                                削除する
+                            </x-danger-button>
+                        </div>
+                    </form>
+                </x-modal>
+            </div>
 
             <x-pagination :paginate-view="$books->paginateView" />
         </x-main-content-panel>
     </x-main-content-area>
-
-    <x-modal name="confirm-delete">
-        <div class="p-6">削除？</div>
-    </x-modal>
 </x-app-layout>
