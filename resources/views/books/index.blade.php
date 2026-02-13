1dc932ff0499a08d6c24674e36a87de087b52477
jounouchi666 <trueguy666@gmail.com>
2026-02-13 14:30:15 +0900

    ナビゲーションボタンのコンポーネント化

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -69,31 +69,18 @@
                                     <td class="p-2 text-gray-800 dark:text-gray-200">
                                         @if ($book->canUpdate)
                                         <div>
-                                            <a class="hover:text-neutral-500 transition-colors" href="{{ route('books.edit', ['id' => $book->id]) }}">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-pen-icon lucide-square-pen"><path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z"/></svg>
-                                            </a>
+                                            <x-edit-icon-link href="{{ route('books.edit', ['id' => $book->id]) }}" />
                                         </div>
                                         @endif
                                     </td>
                                     <td class="p-2 text-gray-800 dark:text-gray-200">
                                         @if ($book->canDelete)
                                         <div>
-                                            <button
-                                                class="hover:text-neutral-500 transition-colors"
-                                                type="button"
-                                                x-on:click="
-                                                    $dispatch('open-book-delete-modal', {
-                                                        action: '{{ route('books.delete', ['id' => $book->id]) }}',
-                                                        title: '{{ $book->title }}'
-                                                    });
-                                                    $dispatch('open-modal', 'confirm-delete');
-                                                "
-                                            >
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-icon lucide-trash">
-                                                    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/>
-                                                    <path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
-                                                </svg>
-                                            </button>
+                                            <x-delete-icon-button
+                                                :title="$book->title"
+                                                :action="route('books.delete', ['id' => $book->id])"
+                                                event="open-book-delete-modal"
+                                            />
                                         </div>
                                         @endif
                                     </td>
