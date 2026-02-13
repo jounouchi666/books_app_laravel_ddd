6170c4ba4fc8b8db8f412ed3d1366e9840bf8499
jounouchi666 <trueguy666@gmail.com>
2026-02-13 17:06:50 +0900

    404ページ作成

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <div class="flex justify-center items-center min-h-[70vh]">
+        <div class="text-center">
+
+            <h1 class="text-4xl font-bold mb-4">
+                404 - {{ __('Page Not Found') }}
+            </h1>
+
+            <p class="mb-6 text-gray-600 dark:text-gray-300">
+                {{ __('The page you are looking for could not be found. It may have been moved, deleted, or the URL may be incorrect.') }}
+            </p>
+
+        </div>
+    </div>
+</x-app-layout>
