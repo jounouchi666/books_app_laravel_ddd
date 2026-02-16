94bedcc40f3dbaf3555c0cebf44cabe49f5df42e
jounouchi666 <trueguy666@gmail.com>
2026-02-16 15:52:51 +0900

    UserServiceProvider追加

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -6,5 +6,6 @@ return [
     App\Providers\BookServiceProvider::class,
     App\Providers\CategoryServiceProvider::class,
     App\Providers\PaginationProvider::class,
+    App\Providers\UserServiceProvider::class,
     App\Providers\ViewServiceProvider::class,
 ];
