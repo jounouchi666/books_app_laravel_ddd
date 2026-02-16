8cb4004648446bbdb71dfd6387b535fd30d38cad
jounouchi666 <trueguy666@gmail.com>
2026-02-16 15:58:51 +0900

    プロパティ追加：isAdmin allUsersの非許容化 ビルドメソッド追加 配列化メソッドの管理者用ロジック修正

--- app/Application/Book/DTO/BookUIQuery.php
+++ app/Application/Book/DTO/BookUIQuery.php
@@ -2,22 +2,51 @@
 
 namespace App\Application\Book\DTO;
 
+use App\Application\Book\Query\ListBookQuery;
+
 /**
  * DTO
  * BookUIQuery
  * 
  * クエリパラメータ用
+ * 
+ * null = 管理者でないため非表示
  */
 final class BookUIQuery
 {
     public function __construct(
+        public readonly bool $isAdmin,
         public readonly ?int $userId,
-        public readonly ?bool $allUser,
+        public readonly bool $allUsers,
         public readonly ?string $sort,
         public readonly ?string $direction,
         public readonly ?string $trashType
     ) {}
-
+    
+    /**
+     * ListBookQueryから生成
+     *
+     * @param  ListBookQuery $query
+     * @param  bool $isAdmin
+     * @return self
+     */
+    public static function fromQuery(ListBookQuery $query, bool $isAdmin): self
+    {
+        return new self(
+            $isAdmin,
+            $isAdmin ? $query->userId : null,
+            $query->allUsers,
+            $query->sort,
+            $query->direction,
+            $isAdmin ? $query->trashType : null
+        );
+    }
+    
+    /**
+     * クエリを配列化
+     *
+     * @return array
+     */
     public function toQueryArray(): array
     {
         $queryArray = [
@@ -25,17 +54,19 @@ final class BookUIQuery
             'direction' => $this->direction,
         ];
 
+        if(!$this->isAdmin) return $queryArray;
+
         // 管理者用
-        if (!is_null($this->userId)) {
+        if($this->allUsers) {
             $queryArray = [
-                'user_id' => $this->userId,
+                'all_users' => 1,
                 ...$queryArray
             ];
         }
 
-        if (!is_null($this->allUser)) {
+        if ($this->isAdmin && !is_null($this->userId)) {
             $queryArray = [
-                'all_user' => $this->allUser,
+                'user_id' => $this->userId,
                 ...$queryArray
             ];
         }
