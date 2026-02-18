b4b5c8e9587e37839cb72f4046693dffbbd4cba0
jounouchi666 <trueguy666@gmail.com>
2026-02-18 17:33:54 +0900

    継承追加

--- app/Application/Category/DTO/CategoryUIQuery.php
+++ app/Application/Category/DTO/CategoryUIQuery.php
@@ -2,26 +2,52 @@
 
 namespace App\Application\Category\DTO;
 
+use App\Application\Category\Query\ListCategoryQuery;
+use App\Application\UI\Query\UIQuery;
+
 /**
  * DTO
  * CategoryUIQuery
  * 
  * クエリパラメータ用
  */
-final class CategoryUIQuery
+final class CategoryUIQuery extends UIQuery
 {
     public function __construct(
-        public readonly ?string $sort,
-        public readonly ?string $direction,
-        public readonly ?string $trashType
-    ) {}
+        ?string $sort,
+        ?string $direction,
+        ?string $trashType
+    ) {
+        parent::__construct($sort, $direction, $trashType);
+    }
 
+    /**
+     * ListCategoryQueryから生成
+     *
+     * @param  ListCategoryQuery $query
+     * @param  bool $isAdmin
+     * @return 
+     */
+    public static function fromQuery(ListCategoryQuery $query): self
+    {
+        return new self(
+            $query->sort,
+            $query->direction,
+            $query->trashType
+        );
+    }
+
+    /**
+     * クエリを配列化
+     *
+     * @return array
+     */
     public function toQueryArray(): array
     {
-        return array_filter([
+        return [
+            'trash_type' => $this->trashType,
             'sort' => $this->sort,
             'direction' => $this->direction,
-            'trash_type' => $this->trashType,
-        ]);
+        ];
     }
 }
