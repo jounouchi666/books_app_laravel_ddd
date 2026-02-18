328e98bc5db340dc5ec7f6c7023c9d8ed9c0661d
jounouchi666 <trueguy666@gmail.com>
2026-02-18 17:29:36 +0900

    インターフェース適用 メソッドの実装

--- app/Application/Book/DTO/BookUIQuery.php
+++ app/Application/Book/DTO/BookUIQuery.php
@@ -3,6 +3,7 @@
 namespace App\Application\Book\DTO;
 
 use App\Application\Book\Query\ListBookQuery;
+use App\Application\UI\Query\HasUserFilter;
 use App\Application\UI\Query\UIQuery;
 
 /**
@@ -11,8 +12,27 @@ use App\Application\UI\Query\UIQuery;
  * 
  * クエリパラメータ用
  */
-final class BookUIQuery extends UIQuery
+final class BookUIQuery extends UIQuery implements HasUserFilter
 {   
+    public readonly bool $isAdmin;
+    public readonly ?int $userId;
+    public readonly bool $allUsers;
+
+    public function __construct(
+        bool $isAdmin,
+        ?int $userId,
+        bool $allUsers,
+        ?string $sort,
+        ?string $direction,
+        ?string $trashType
+    ) {
+        parent::__construct($sort, $direction, $trashType);
+
+        $this->isAdmin = $isAdmin;
+        $this->userId = $userId;
+        $this->allUsers = $allUsers;
+    }
+
     /**
      * ListBookQueryから生成
      *
@@ -30,5 +50,66 @@ final class BookUIQuery extends UIQuery
             $query->direction,
             $isAdmin ? $query->trashType : null
         );
+    }
+    
+    /**
+     * Getter
+     * userId
+     *
+     * @return int
+     */
+    public function userId(): ?int
+    {
+        return $this->userId;
+    }
+    
+    /**
+     * Getter
+     * allUsers
+     *
+     * @return bool
+     */
+    public function allUsers(): bool
+    {
+        return $this->allUsers;
+    }
+
+    /**
+     * クエリを配列化
+     *
+     * @return array
+     */
+    public function toQueryArray(): array
+    {
+        $queryArray = [
+            'sort' => $this->sort,
+            'direction' => $this->direction,
+        ];
+
+        if(!$this->isAdmin) return $queryArray;
+
+        // 管理者用
+        if($this->allUsers) {
+            $queryArray = [
+                'all_users' => 1,
+                ...$queryArray
+            ];
+        }
+
+        if ($this->isAdmin && !is_null($this->userId)) {
+            $queryArray = [
+                'user_id' => $this->userId,
+                ...$queryArray
+            ];
+        }
+
+        if (!is_null($this->trashType)) {
+            $queryArray = [
+                'trash_type' => $this->trashType,
+                ...$queryArray
+            ];
+        }
+
+        return $queryArray;
     }
 }
