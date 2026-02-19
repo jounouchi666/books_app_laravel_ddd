3d69019dfd6a47bc520be99fd747a7c02bf1cd85
jounouchi666 <trueguy666@gmail.com>
2026-02-19 11:23:18 +0900

    レイアウト調整

--- resources/views/components/admin-filter-forms.blade.php
+++ resources/views/components/admin-filter-forms.blade.php
@@ -1,5 +1,5 @@
 @if ($isAdmin)
-<div class="mt-6 p-5 border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-900 rounded-lg">
+<div class="p-5 border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-900 rounded-lg">
 
     <div class="flex items-center justify-between mb-4">
         <div class="text-xs font-semibold tracking-wider uppercase text-gray-500 dark:text-gray-400">
@@ -7,7 +7,7 @@
         </div>
     </div>
 
-    <div class="flex flex-col items-end gap-6 lg:flex-row lg:items-start lg:justify-between">
+    <div class="flex flex-col items-end gap-6 lg:flex-row lg:items-start">
 
         <div class="flex flex-col gap-2">
             <div class="text-xs text-gray-500 dark:text-gray-400">
