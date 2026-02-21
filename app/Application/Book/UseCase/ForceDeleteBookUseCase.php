5150807047d15d0e593cb94e32e8b592e48c4cbc
jounouchi666 <trueguy666@gmail.com>
2026-02-21 12:11:03 +0900

    レスポンスのDTO化

--- app/Application/Book/UseCase/ForceDeleteBookUseCase.php
+++ app/Application/Book/UseCase/ForceDeleteBookUseCase.php
@@ -3,7 +3,11 @@
 namespace App\Application\Book\UseCase;
 
 use App\Application\Auth\AuthorizationService;
+use App\Application\Auth\CurrentUserProvider;
 use App\Application\Auth\Permission\BookPermission;
+use App\Application\Book\DTO\ActionBookResponse;
+use App\Application\Book\DTO\BookUIQuery;
+use App\Application\Book\Query\ListBookQuery;
 use App\Domain\Book\Exception\BookNotFoundException;
 use App\Domain\Book\Repository\BookRepositoryInterface;
 use App\Domain\Book\ValueObject\BookId;
@@ -16,17 +20,21 @@ class ForceDeleteBookUseCase
 {
     public function __construct(
         private BookRepositoryInterface $bookRepository,
-        private AuthorizationService $authorizationService
+        private AuthorizationService $authorizationService,
+        private CurrentUserProvider $currentUserProvider
     ) {}
     
     /**
      * 実行
      *
      * @param  int $id
-     * @return void
+     * @return ActionBookResponse
      */
-    function execute(int $id): void
-    {       
+    function execute(int $id, ListBookQuery $query): ActionBookResponse
+    {
+        $currentUser = $this->currentUserProvider->currentUser();
+        $isAdmin = $currentUser->isAdmin();
+
         $bookId = new BookId($id);
         $book = $this->bookRepository->findById($bookId);
         
@@ -34,9 +42,16 @@ class ForceDeleteBookUseCase
 
         // 認可
         $this->authorizationService->authorize(
-            BookPermission::delete($book)
+            BookPermission::forceDelete($book)
         );
 
         $this->bookRepository->forceDelete($bookId);
+
+        $bookUIQuery = BookUIQuery::fromQuery($query, $isAdmin);
+
+        return new ActionBookResponse(
+            $bookUIQuery,
+            '復元しました'
+        );
     }
 }
