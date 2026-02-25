72e28704a499cf1009e45d79b59f0a47a556c4bd
jounouchi666 <trueguy666@gmail.com>
2026-02-25 16:22:15 +0900

    判定メソッド追加

--- app/Application/UI/DTO/TrashActionType.php
+++ app/Application/UI/DTO/TrashActionType.php
@@ -7,4 +7,14 @@ enum TrashActionType: string
     case Delete = 'delete';
     case Restore = 'restore';
     case None = 'none';
+
+    public function isDelete(): bool
+    {
+        return $this === self::Delete;
+    }
+
+    public function isRestore(): bool
+    {
+        return $this === self::Restore;
+    }
 }
