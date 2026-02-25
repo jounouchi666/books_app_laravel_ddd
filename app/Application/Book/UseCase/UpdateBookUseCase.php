ba7e30fc37910f152f959128c2649c56ed6aeb51
jounouchi666 <trueguy666@gmail.com>
2026-02-25 16:39:29 +0900

    フォームデータの取得をDTO経由に変更

--- app/Application/Book/UseCase/UpdateBookUseCase.php
+++ app/Application/Book/UseCase/UpdateBookUseCase.php
@@ -3,11 +3,14 @@
 namespace App\Application\Book\UseCase;
 
 use App\Application\Auth\AuthorizationService;
+use App\Application\Auth\CurrentUserProvider;
 use App\Application\Auth\Permission\BookPermission;
+use App\Application\Book\DTO\SaveBookDto;
 use App\Domain\Book\Entity\Book;
 use App\Domain\Book\Exception\BookNotFoundException;
 use App\Domain\Book\Repository\BookRepositoryInterface;
 use App\Domain\Book\ValueObject\BookId;
+use App\Domain\Book\ValueObject\BookReadingStatus;
 use App\Domain\Book\ValueObject\BookTitle;
 use App\Domain\Shared\ValueObject\CategoryId;
 use App\Domain\Shared\ValueObject\UserId;
@@ -20,36 +23,38 @@ class UpdateBookUseCase
 {
     public function __construct(
         private BookRepositoryInterface $bookRepository,
-        private AuthorizationService $authorizationService
+        private AuthorizationService $authorizationService,
+        private CurrentUserProvider $currentUserProvider
     ) {}
     
     /**
      * 実行
      *
      * @param  int $id
-     * @param  string $title
-     * @param  int $userId
-     * @param  int $categoryId
+     * @param  SaveBookDto $book
      * @return Book
      */
-    function execute(int $id, string $title, int $userId, ?int $categoryId): Book
+    function execute(int $id, SaveBookDto $book): Book
     {
         $bookId = new BookId($id);
-        $book = $this->bookRepository->findById($bookId);
+        $bookEntity = $this->bookRepository->findById($bookId);
+
+        $currentUser = $this->currentUserProvider->currentUser();
         
-        if (is_null($book)) throw new BookNotFoundException($bookId);
+        if (is_null($bookEntity)) throw new BookNotFoundException($bookId);
         
         // 認可
         $this->authorizationService->authorize(
-            BookPermission::update($book)
+            BookPermission::update($bookEntity)
         );
 
-        $book->changeTitle(new BookTitle($title));
-        $book->changeUser(new UserId($userId));
-        $book->changeCategory(
-            is_null($categoryId) ? null : new CategoryId($categoryId)
+        $bookEntity->changeTitle(new BookTitle($book->title));
+        $bookEntity->changeUser(new UserId($currentUser->id()->value()));
+        $bookEntity->changeCategory(
+            is_null($book->categoryId) ? null : new CategoryId($book->categoryId)
         );
+        $bookEntity->chengeStatus($book->readingStatus);
 
-        return $this->bookRepository->save($book);
+        return $this->bookRepository->save($bookEntity);
     }
 }
