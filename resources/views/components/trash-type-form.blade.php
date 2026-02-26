00132df5152131f62e011da220d49e0b157d984f
jounouchi666 <trueguy666@gmail.com>
2026-02-26 16:58:52 +0900

    CSRFトークンの埋め込み追加

--- resources/views/components/trash-type-form.blade.php
+++ resources/views/components/trash-type-form.blade.php
@@ -4,6 +4,7 @@
     x-data='@json(["checked" => $selectedTrashType])'
     class="flex items-center"
 >
+    @csrf
     <x-form-query-input :params="$currentQueries" />
 
     <div class="inline-flex h-10">
