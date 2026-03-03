594b2e5e9339d76bf3b25afb3241b068a5f2094d
jounouchi666 <trueguy666@gmail.com>
2026-03-03 17:21:54 +0900

    ダッシュボード用partial作成「削除済み件数」

---
+++ resources/views/dashboard/partials/admin/deleted.blade.php
@@ -0,0 +1,19 @@
+<section>
+    <header>
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+            削除済み件数
+        </h2>
+    </header>
+
+    <dl class="mt-4 space-y-3">
+        <div class="flex justify-between">
+            <dt class="text-sm text-neutral-600">書籍</dt>
+            <dd class="font-semibold text-red-600">20</dd>
+        </div>
+
+        <div class="flex justify-between">
+            <dt class="text-sm text-neutral-600">カテゴリー</dt>
+            <dd class="font-semibold text-red-600">10</dd>
+        </div>
+    </dl>
+</section>
