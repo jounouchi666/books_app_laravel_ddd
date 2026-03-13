caafcc005e95700220f91bafa13ca2105a645cab
jounouchi666 <trueguy666@gmail.com>
2026-03-13 16:17:06 +0900

    Grant execution to storage

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,5 +1,8 @@
 echo "Running deployment scripts..."
 
+# Grant execution to storage
+chmod -R 777 storage bootstrap/cache
+
 # migration & seeder
 php artisan migrate:fresh --seed --force
 
@@ -7,3 +10,5 @@ php artisan migrate:fresh --seed --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
+
+echo "Deployment scripts finished."
