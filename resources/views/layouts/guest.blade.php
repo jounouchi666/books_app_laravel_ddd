c4e18203d62113e6c19542b4fa63d69eae49283a
jounouchi666 <trueguy666@gmail.com>
2026-03-15 16:03:32 +0900

    faviconの設定

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,6 +7,9 @@
 
         <title>{{ config('app.display_name', config('app.name', 'BooksApp')) }}</title>
 
+        <!-- Favicon -->
+        <link rel="icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
